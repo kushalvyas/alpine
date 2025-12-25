f040f556b871812ff59baba84d61e64fa354984f
daniel360kim <nmrs.thrust@gmail.com>
2025-12-24 22:07:32 -0700

    improved verbose messages

--- alpine/utils/grid_search.py
+++ alpine/utils/grid_search.py
@@ -31,29 +31,37 @@ class GridSearch:
         return [dict(zip(keys, v)) for v in itertools.product(*values)]
     
     
-    def run(self, coords, signal, verbose=True):
+    def run(self, coords, signal, metric_trackers=None, verbose=True):
         self.results = []
         model_param_combos = self._generate_combos(self.model_params)
         compile_param_combos = self._generate_combos(self.compile_params)
         
         num_iterations = len(model_param_combos) * len(compile_param_combos) * len(self.n_iters)
-        
-        print(f"Running grid search with {num_iterations} total runs.")
-        
+        if verbose:
+            print(f"Starting grid search with {num_iterations} candidates on {self.device}.")
+    
         for model_params in model_param_combos:
             for compile_params in compile_param_combos:
                 for n_iters_param in self.n_iters:
                     model = self.model_class(**model_params).to(self.device)
                     model.compile(**compile_params)
-                    print(f"Training with model params: {model_params}, compile params: {compile_params}, n_iters param: {n_iters_param}")
+                    if verbose:
+                       config_str = f"Arch: {model_params} | Optim: {compile_params} | Iters: {n_iters_param}"
+                       print(f"Running configuration: {config_str}")
                     result = model.fit_signal(
                         input = coords,
                         signal = signal,
-                        enable_tqdm = True,
+                        enable_tqdm = verbose,
                         return_features = False,
                         track_loss_history = False,
                         n_iters=n_iters_param,
+                        metric_trackers=metric_trackers,
                     )
+                    
+                    final_loss = result['loss']
+                    if verbose:
+                        print(f"--> Loss: {final_loss:.6f}")
+                        
                     self.results.append({
                         'model_params': model_params,
                         'compile_params': compile_params,
